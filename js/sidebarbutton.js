7d1edd12e5644cd1292f349df9c77867fc15ea1c
achmadsutardi441953 <achmadsutardi441953@gmail.com>
2020-05-31 14:59:46 +0700

    adding files

---
+++ js/sidebarbutton.js
@@ -0,0 +1,10 @@
+ // Close menu
+ $("#close-menu").click(function(e) {
+     e.preventDefault();
+     $("#sidebar-wrapper").toggleClass("active");
+ });
+ // Open menu
+ $("#menu-toggle").click(function(e) {
+     e.preventDefault();
+     $("#sidebar-wrapper").toggleClass("active");
+ });
